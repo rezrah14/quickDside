972b818c179ae59f73162d94b62dfc6f0eb8a7c5
mrahmanian <mrrsh14@gmail.com>
2023-02-05 23:22:17 -0700

    add edit/update & destroy + add links in the index page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,6 +14,7 @@ class ProjectsController < ApplicationController
   end
 
   def new
+    @project = Project.new
   end
 
   def create
@@ -26,6 +27,33 @@ class ProjectsController < ApplicationController
       render 'new', status: :unprocessable_entity
     end
     
+  end
+
+  def edit
+    id = params[:id]
+    if Project.exists?(id)
+      @project = Project.find(id)
+    end
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(params.require(:project).permit(:title, :description))
+      flash[:notice] = "Project was editted successfully."
+      redirect_to @project
+      # ^ is the shortened version of : redirect_to project_path(@project.id)
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    id = params[:id]
+    if Project.exists?(id)
+      @project = Project.find(id)
+      @project.destroy
+      redirect_to projects_path
+    end
   end
 
 end
