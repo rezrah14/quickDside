3244b6b470641960a1f653b53297228226496695
mrahmanian <mrrsh14@gmail.com>
2023-04-05 07:58:48 -0600

    add password to users + add signup form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   resources :projects
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'user#create'
+  resources :users, except: [:new]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_03_025444) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_04_040917) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -28,6 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_03_025444) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
