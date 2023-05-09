e884122a3098025a7014d919e8d8904b64e41ff7
mrahmanian <mrrsh14@gmail.com>
2023-05-09 13:12:21 -0600

    modified gmail server settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,21 +91,23 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address              => "smtp.elasticemail.com",
+  #   :port                 => 2525,
+  #   :domain               => "quickdside.onrender.com",
+  #   :user_name            => ENV["ELASTIC_EMAIL_USERNAME"],
+  #   :password             => ENV["ELASTIC_EMAIL_PASSWORD"],
+  #   :authentication       => :login,
+  #   :enable_starttls_auto => true
+  # }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    # :address              => "smtp.elasticemail.com",
-    # :port                 => 2525,
-    # :domain               => "quickdside.onrender.com",
-    # :user_name            => ENV["ELASTIC_EMAIL_USERNAME"],
-    # :password             => ENV["ELASTIC_EMAIL_PASSWORD"],
-    # :authentication       => :login,
-    # :enable_starttls_auto => true
-
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :domain               => "gmail.com",
-    :user_name            => ENV["ELASTIC_EMAIL_USERNAME"],
-    :password             => ENV["QUICKDSIDE_EMAIL_PASSWORD"],
+    :user_name            => ENV["GMAIL_USERNAME"],
+    :password             => ENV["GMAIL_PASSWORD"],
     :authentication       => :plain,
     :enable_starttls_auto => true
   }
