86ff9637bd6336a35f01c8875f51bfb653ecb9ab
mrahmanian <mrrsh14@gmail.com>
2023-05-10 08:40:22 -0600

    setup sendgrid email server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,13 +104,12 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => ENV["GMAIL_USERNAME"],
-    :password             => ENV["GMAIL_PASSWORD"],
-    :authentication       => :plain,
-    :enable_starttls_auto => true
+    address:              'smtp.sendgrid.net',
+    port:                 587,
+    user_name:            'apikey',
+    password:             ENV['SENDGRID_API_KEY'],
+    authentication:       'plain',
+    enable_starttls_auto: true
   }
-
   config.action_mailer.default_url_options = { host: 'quickdside.onrender.com' }
 end
